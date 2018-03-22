46760b0b9b7a64198d94970b8e826617ad7f81ae
onepve <onepve@gmail.com>
2018-03-22 20:58:21 +0800

    探针

--- ServerStatus/status.sh
+++ ServerStatus/status.sh
@@ -61,7 +61,7 @@ Download_Server_Status_server(){
 	cd "/usr/local"
 	wget -N --no-check-certificate "https://github.com/cppla/ServerStatus/archive/master.zip"
 	[[ ! -e "master.zip" ]] && echo -e "${Error} ServerStatus 服务端下载失败 !" && exit 1
-	unzip master.zip && rm -rf master.zip && rm -rf ServerStatus-master
+	rm -rf ServerStatus-master && unzip master.zip && rm -rf master.zip
 	[[ ! -e "ServerStatus-master" ]] && echo -e "${Error} ServerStatus 服务端解压失败 !" && exit 1
 	if [[ ! -e "${file}" ]]; then
 		mv ServerStatus-master ServerStatus
